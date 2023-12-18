b09e4f64db42aa0b43dfaf9d0b3678875721bac8
brezlow <breezeblow@outlook.com>
2023-12-18 21:11:56 +0800

    use xmake to configure opencv😉

---
+++ src/readJPEG.cpp
@@ -0,0 +1,19 @@
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+
+int main() {
+  std::string imagePath = "assert/rei.jpg";
+  cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
+
+  if (image.empty()) {
+    std::cout << "cannot read image" << std::endl;
+    return -1;
+  }
+
+  cv::imshow("Image", image);
+  cv::waitKey(0);
+
+  return 0;
+}
